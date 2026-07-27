c2abbc629e24f115d5cd1eb22656771850f450ee
Clemens Kaiser <kaisercgn@gmail.com>
2026-07-27 12:47:05 +0200

    style: cleanup footer with modern dark teal theme, Cormorant Garamond typography and clean layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,80 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { HeartPulse, MapPin, Phone, Mail, ExternalLink, ShieldCheck } from 'lucide-react';
+import { MapPin, Phone, Mail, ExternalLink, ShieldCheck, Calendar } from 'lucide-react';
 import './Footer.css';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="footer-root">
+      <div className="footer-accent-line" />
       <div className="container footer-top">
         <div className="footer-grid">
-          {/* Col 1: About */}
+          {/* Col 1: Brand & Certification */}
           <div className="footer-col brand-col">
             <Link to="/" className="footer-logo">
-              <img src="./logo.png" alt="Mamma MRT Zentrum" className="footer-logo-img" />
-              <div>
+              <img src="./logo.png" alt="KMZ Mamma MRT Zentrum" className="footer-logo-img" />
+              <div className="footer-logo-text">
                 <span className="footer-logo-title">KMZ</span>
                 <span className="footer-logo-sub">Kaiser MR-Mammographie Zentrum</span>
               </div>
             </Link>
             <p className="footer-desc">
-              KMZ – Ihr hochspezialisiertes Zentrum für präzise Mamma-MRT Diagnostik und innovative Brustkrebsvorsorge. Unter der wissenschaftlichen Leitung von Prof. Dr. med. Clemens Kaiser.
+              Spezialisiertes Zentrum für hochpräzise Mamma-MRT Diagnostik &amp; Früherkennung. Wiss. Leitung: Prof. Dr. med. Clemens Kaiser.
             </p>
-            <div className="footer-certification">
-              <ShieldCheck size={18} className="cert-icon" />
-              <span>Zertifiziertes Zentrum für Früherkennung</span>
+            <div className="footer-badges">
+              <div className="footer-badge-item">
+                <ShieldCheck size={16} className="badge-icon" />
+                <span>TK-Vertragspartner &amp; QMRM zertifiziert</span>
+              </div>
             </div>
           </div>
 
-          {/* Col 2: Navigation Mamma-MRT */}
+          {/* Col 2: Diagnostik & Mamma-MRT */}
           <div className="footer-col">
-            <h4 className="footer-heading">Mamma-MRT & Diagnostik</h4>
+            <h4 className="footer-heading">Mamma-MRT Diagnostik</h4>
             <ul className="footer-links">
-              <li><Link to="/brustkrebsvorsorge">Für Patientinnen</Link></li>
-              <li><Link to="/MR-Mammographie">Mamma-MRT Ablauf</Link></li>
-              <li><Link to="/qualitaetssicherung">Qualitätssicherung & QMRM</Link></li>
-              <li><Link to="/fuer-gynaekolog-innen">Für Gynäkolog:innen</Link></li>
+              <li><Link to="/MR-Mammographie">Mamma-MRT Ablauf &amp; Indikationen</Link></li>
+              <li><Link to="/qualitaetssicherung">Qualitätssicherung &amp; QMRM</Link></li>
+              <li><Link to="/brustkrebsvorsorge">Informationen für Patientinnen</Link></li>
+              <li><Link to="/fuer-gynaekolog-innen">Zuweiser &amp; Gynäkolog:innen</Link></li>
             </ul>
           </div>
 
-          {/* Col 3: Über uns & Service */}
+          {/* Col 3: Zentrum & Service */}
           <div className="footer-col">
-            <h4 className="footer-heading">Über uns & Service</h4>
+            <h4 className="footer-heading">Zentrum &amp; Service</h4>
             <ul className="footer-links">
-              <li><Link to="/ueber-uns">Das Zentrum & Team</Link></li>
-              <li><Link to="/wissenschaft-forschung">Wissenschaft & Forschung</Link></li>
-              <li><Link to="/terminanfrage">Termin online anfragen</Link></li>
+              <li><Link to="/ueber-uns">Das Zentrum &amp; Ärzteteam</Link></li>
+              <li><Link to="/wissenschaft-forschung">Wissenschaft &amp; Forschung</Link></li>
+              <li><Link to="/terminanfrage">Online-Terminanfrage</Link></li>
               <li><Link to="/faqs">Häufige Fragen (FAQ)</Link></li>
-              <li><Link to="/kontakt">Kontakt & Anfahrt</Link></li>
             </ul>
           </div>
 
-          {/* Col 4: Contact & Location */}
+          {/* Col 4: Praxis & Kontakt */}
           <div className="footer-col contact-col">
-            <h4 className="footer-heading">Standort & Kontakt</h4>
+            <h4 className="footer-heading">Praxis &amp; Kontakt</h4>
             <ul className="contact-list">
               <li>
-                <MapPin size={18} className="contact-icon" />
-                <span>KMZ – Kaiser MR-Mammographie Zentrum<br />Hauptstandort Köln / Diagnostik<br />50667 Köln</span>
+                <MapPin size={16} className="contact-icon" />
+                <span>KMZ Diagnostikzentrum Köln<br />50667 Köln</span>
               </li>
               <li>
-                <Phone size={18} className="contact-icon" />
-                <span>+49 (0) 221 / 123 456-0</span>
+                <Phone size={16} className="contact-icon" />
+                <a href="tel:02211234567" className="contact-link">0221 / 123 45 67</a>
               </li>
               <li>
-                <Mail size={18} className="contact-icon" />
-                <span>info@kmz-brustvorsorge.de</span>
+                <Mail size={16} className="contact-icon" />
+                <a href="mailto:info@kmz-brustvorsorge.de" className="contact-link">info@kmz-brustvorsorge.de</a>
               </li>
             </ul>
-            <a
-              href="https://maps.google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn btn-outline footer-map-btn"
-            >
-              <span>Routenplaner öffnen</span>
-              <ExternalLink size={14} />
-            </a>
+            <div className="footer-cta-row">
+              <Link to="/terminanfrage" className="footer-btn footer-btn-primary">
+                <Calendar size={14} />
+                <span>Termin buchen</span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
